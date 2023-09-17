65a675072f547f8fe702108147a639ad98eaac1e
Harok <41049683+dorianharok@users.noreply.github.com>
2023-09-17 21:07:43 +0900

    [FEAT] 로그인 회원가입 구현#6

--- src/main/java/com/sixbald/webide/exception/ExceptionController.java
+++ src/main/java/com/sixbald/webide/exception/ExceptionController.java
@@ -4,8 +4,13 @@ import com.sixbald.webide.common.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
@@ -25,5 +30,17 @@ public class ExceptionController {
         log.error("error occur : {}", e.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(Response.error(e.getMessage()));
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Response<Void>>> argsValidHandler(MethodArgumentNotValidException e) {
+        log.error("error occur: {}" , e.getStackTrace());
+        log.error("error occur : {}", e.toString());
+        List<Response<Void>> errors = new ArrayList<>();
+        e.getFieldErrors().stream()
+                .forEach(error -> errors.add(Response.error(error.getField(), error.getDefaultMessage())));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errors);
     }
 }
