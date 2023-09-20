a13a6bf62136a6b780e9eb4977d1f0aa496e2363
Jisoo Kim <ooSoo2Soo2oo@gmail.com>
2023-09-20 15:06:09 +0900

    #27 #28 PathUtils 재수정. 테스트 미완료

--- src/main/java/com/sixbald/webide/common/PathUtils.java
+++ src/main/java/com/sixbald/webide/common/PathUtils.java
@@ -2,7 +2,11 @@ package com.sixbald.webide.common;
 
 public class PathUtils {
 
+    private PathUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static String absolutePath(Long userId, String path){
-        return "/root/" + userId + "/src" + path;
+        return "/root/" + userId + path;
     }
 }
