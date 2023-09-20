8f55db79f2939a84057ddcea2625c48c8c5b6a2f
yaejingo <yaejingo@gmail.com>
2023-09-20 14:47:15 +0900

    [FEAT] 폴더 이름 변경하기 #22

--- src/main/java/com/sixbald/webide/common/PathUtils.java
+++ src/main/java/com/sixbald/webide/common/PathUtils.java
@@ -1,9 +1,7 @@
 package com.sixbald.webide.common;
 
-import lombok.Getter;
-
 public class PathUtils {
-    public static String absolutePath(Long userId, String path){
-        return "/root/" + userId + path;
+    public static String absolutePath(Long userId, String path) {
+        return "/root/" + userId + "/src" + path;
     }
 }
