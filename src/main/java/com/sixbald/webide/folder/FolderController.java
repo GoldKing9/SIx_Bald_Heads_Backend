f9d4a03caa66d3f595198ddc7bc4bc0019485514
donysony <doffltm9812@gmail.com>
2023-09-21 00:13:06 +0900

    [refactor] 폴더 이동 api 버전추가 #25

--- src/main/java/com/sixbald/webide/folder/FolderController.java
+++ src/main/java/com/sixbald/webide/folder/FolderController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/v1/file-tree")
 public class FolderController {
     private final FolderService folderService;
 
     //폴더 이동
-    @PutMapping("/folders/path")
+    @PutMapping("/folder/move")
     public Response<Void> moveFolder(
             @RequestBody RequestFolderDTO requestFolderDTO,
             @AuthenticationPrincipal LoginUser loginUser
