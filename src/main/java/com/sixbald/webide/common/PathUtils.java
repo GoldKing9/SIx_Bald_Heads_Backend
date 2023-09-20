545f192b66dbb0c8550a4157745a72a13e24be4d
yaejingo <yaejingo@gmail.com>
2023-09-20 15:38:42 +0900

    [FEAT] 파일 이동하기 #23

--- src/main/java/com/sixbald/webide/common/PathUtils.java
+++ src/main/java/com/sixbald/webide/common/PathUtils.java
@@ -2,6 +2,10 @@ package com.sixbald.webide.common;
 
 public class PathUtils {
     public static String absolutePath(Long userId, String path) {
-        return "/root/" + userId + "/src" + path;
+        return "/root/" + userId + path;
+    }
+
+    private PathUtils() {
+        throw new IllegalStateException("Utility class");
     }
 }
