d18017437c453b5f027a78879c2b93e9dee99f4e
Jisoo Kim <ooSoo2Soo2oo@gmail.com>
2023-09-20 14:47:16 +0900

    #27 #28 PathUtils에 /src 추가

--- src/main/java/com/sixbald/webide/common/PathUtils.java
+++ src/main/java/com/sixbald/webide/common/PathUtils.java
@@ -3,6 +3,6 @@ package com.sixbald.webide.common;
 public class PathUtils {
 
     public static String absolutePath(Long userId, String path){
-        return "/root/" + userId + path;
+        return "/root/" + userId + "/src" + path;
     }
 }
