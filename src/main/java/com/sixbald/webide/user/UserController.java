743764b4c74707ea093d13b41950d947bd884d69
donysony <doffltm9812@gmail.com>
2023-10-08 19:22:33 +0900

    [fix] 이미지 업로드 미디어타입 추가 #5
    기존에 이미지경로를 스웨거에서 application/json으로 받고 있었다. mutipart-formdata로 받기 위해 로직을 추가해주었다.
    이미지 경로를 @RequestParam으로 받음으로써, 스웨거는 쿼리스트링과 requestBody 두가지를 요청받도록 되어있었다. 혼란을 방지하기 위해 @RequestPart로 변경해줬다

--- src/main/java/com/sixbald/webide/user/UserController.java
+++ src/main/java/com/sixbald/webide/user/UserController.java
@@ -7,6 +7,10 @@ import com.sixbald.webide.user.dto.request.*;
 import com.sixbald.webide.user.dto.response.UserDTO;
 import com.sixbald.webide.user.dto.response.UserLoginResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,6 +18,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,6 +26,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 
 @Slf4j
@@ -76,10 +83,14 @@ public class UserController {
     }
     //프로필 이미지 수정
 
-    @PutMapping("/profile/image")
+    @PutMapping(
+            value = "/profile/image",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
     @Operation(summary = "프로필 이미지 수정")
     public Response<Void> updateImage(
-            @NotNull @RequestParam MultipartFile imageUrl,
+            @NotNull
+            @RequestPart MultipartFile imageUrl,
             @AuthenticationPrincipal LoginUser loginUser
     ) {
         userService.updateUserProfileImage(loginUser.getUser().getId(), imageUrl);
